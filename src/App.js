d89a44adff9760f9c0dfc5f5b9b71fe8867a339c
PGCranford <PGCranford@gmail.com>
2022-09-11 14:24:34 -0400

    components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Project from './components/Project';
 
 
 
