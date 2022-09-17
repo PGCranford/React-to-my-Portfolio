bef82225a205fb8bd8015ee6677aefbe4ff48687
PGCranford <PGCranford@gmail.com>
2022-09-17 15:09:56 -0400

    project and portfolio restart

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,65 +1,52 @@
 import React, { useState } from "react";
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import Hover from '../Hover'
+import Project from "../Project";
 
-const Portfolio = ({ category }) => {
-    const [currentPhoto, setCurrentPhoto] = useState();
-
-    const [photos] = useState([
-        {
-            name: 'Horiseon',
-            category: 'Project',
-            description: 'First time correcting code that was not working as expected'
-        },
-        {
-            name: 'Rent A Ride',
-            category: 'Project',
-            description: 'Second Group Project using Node and Express, Handlebars, MYSQL and Sequelize'
-        },
-        {
-            name: 'Run Buddy',
-            category: 'Project',
-            description: 'Beginning of my coding journey, basic HTML and CSS'
-        }
-    ]);
-
-    const currentPhotos = photos.filter(photo => photo.category === category);
-
-    const [isHovering, setIsHovering] = useState(false);
-
-    // const handleMouseOver = () => {
-    //     setIsHovering(true);
-    // };
-    const handleMouseOut = () => {
-        setIsHovering(false);
-    };
-
-    const hoverOver = (image, i) => {
-        setCurrentPhoto({ ...image, index: i });
-
+const projects = [
+    {
+        name: "",
+        description: "",
+        imagePath: require("../../assets/images/")
+    },
+    {
+        name: "",
+        description: "",
+        imagePath: require("../../assets/images/")
+    },
+    {
+        name: "",
+        description: "",
+        imagePath: require("../../assets/images/")
+    },
+    {
+        name: "",
+        description: "",
+        imagePath: require("../../assets/images/")
+    },
+    {
+        name: "",
+        description: "",
+        imagePath: require("../../assets/images/")
+    },
+    {
+        name: "",
+        description: "",
+        imagePath: require("../../assets/images/")
     }
+];
 
+const Portfolio = () => {
     return (
-        <div className="flex-row">
-            {isHovering && (
-                <Hover onClose={handleMouseOut} currentPhoto={currentPhoto} />
-            )}
-            <div>
-
-                {currentPhotos.map((image, i) => (
-                    <img
-                        src={require(`../../assets/projects/${category}/${i}.jpg`).default}
-                        alt={image.name}
-                        className="thumbnail mx-1"
-                        isHovering={() => hoverOver(image, i)}
-                        key={image.name}
-
-
-                    />
-                ))};
-            </div>
-
-        </div >
+        <>
+            {projects.map(({ name, description, imagePath }) => (
+                <Project
+                    name={name}
+                    description={description}
+                    imagePath={imagePath}
+                    key={name}
+                />
+            ))}
+        </>
     );
 };
 
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,65 +1,18 @@
-import React, { useState } from "react";
-import { capitalizeFirstLetter } from '../../utils/helpers';
-import Hover from '../Hover'
+import React from "react";
+import styles from "./style.module.css";
 
-const Project = ({ category }) => {
-    const [currentPhoto, setCurrentPhoto] = useState();
-
-    const [photos] = useState([
-        {
-            name: 'Horiseon',
-            category: 'Project',
-            description: 'First time correcting code that was not working as expected'
-        },
-        {
-            name: 'Rent A Ride',
-            category: 'Project',
-            description: 'Second Group Project using Node and Express, Handlebars, MYSQL and Sequelize'
-        },
-        {
-            name: 'Run Buddy',
-            category: 'Project',
-            description: 'Beginning of my coding journey, basic HTML and CSS'
-        }
-    ]);
-
-    const currentPhotos = photos.filter(photo => photo.category === category);
-
-    const [isHovering, setIsHovering] = useState(false);
-
-    // const handleMouseOver = () => {
-    //     setIsHovering(true);
-    // };
-    const handleMouseOut = () => {
-        setIsHovering(false);
-    };
-
-    const hoverOver = (image, i) => {
-        setCurrentPhoto({ ...image, index: i });
-
-    }
+const Project = ({ name, description, imagePath }) => {
 
     return (
-        <div className="flex-row">
-            {isHovering && (
-                <Hover onClose={handleMouseOut} currentPhoto={currentPhoto} />
-            )}
+
+        <div class={styles['project-image']}>
+            <img src={imagePath} alt={description} />
+
             <div>
-
-                {currentPhotos.map((image, i) => (
-                    <img
-                        src={require(`../../assets/projects/${category}/${i}.jpg`).default}
-                        alt={image.name}
-                        className="thumbnail mx-1"
-                        isHovering={() => hoverOver(image, i)}
-                        key={image.name}
-
-
-                    />
-                ))};
+                <h3>{name}</h3>
+                <p> {description} </p>
             </div>
-
-        </div >
+        </div>
     );
 };
 
