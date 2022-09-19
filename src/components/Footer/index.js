2fa405103473001a19972783bc1e5c5a59670452
PGCranford <PGCranford@gmail.com>
2022-09-19 15:40:33 -0400

    portfolio updated

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import gitHub from "../../assets/images/gitHub.png";
-import linkedIn from "../../assets/images/Linked.png";
+import { SocialIcon } from "react-social-icons";
+
 
 
 
@@ -9,10 +9,10 @@ function Footer() {
     return (
         <footer className="flex-row footerMain">
             <h3 id="footer">
-                <a href="https://github.com/PGCranford"> <img src={gitHub} alt="Git Hub Logo"></img></a>
-                <a href="https://www.linkedin.com/in/patrick-cranford-320b70216?trk=people-guest_people_search-card"> <img src={linkedIn} alt="LinkedIn Logo"></img></a>
+                <a href="https://github.com/PGCranford"> <SocialIcon network="github" style={{ height: 25, width: 25 }} alt="Git Hub Logo" /></a>
+                <a href="https://www.linkedin.com/in/patrick-cranford-320b70216?trk=people-guest_people_search-card"> < SocialIcon network="linkedin" style={{ height: 25, width: 25 }} alt="LinkedIn Logo" /></a>
             </h3>
-        </footer>
+        </footer >
     )
 }
 
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,36 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
 import Project from "../Project";
 import styles from "./style.module.css";
 
 const projects = [
     {
         name: "Rent A Ride ",
-        links: "https://github.com/PGCranford/Rent-A-Ride-",
+        links: "https://github.com/PGCranford/Rent-A-Ride- https://sleepy-cove-56868.herokuapp.com/",
         imagePath: require("../../assets/projects/0.jpg")
     },
     {
         name: "Brews and Clues",
-        links: "",
+        links: "https://github.com/PGCranford/Brew-s-Clues- https://pgcranford.github.io/Brew-s-Clues-/ ",
         imagePath: require("../../assets/projects/1.jpg")
     },
     {
         name: "Note Taker",
-        links: "",
+        links: "https://github.com/PGCranford/Take-a-note-Leave-a-note https://dry-cove-43814.herokuapp.com/ ",
         imagePath: require("../../assets/projects/2.jpg")
     },
     {
-        name: "",
-        links: "",
+        name: "Horiseon",
+        links: "https://github.com/PGCranford/Horiseon-Code-Refactor https://pgcranford.github.io/MOD1-/",
         imagePath: require("../../assets/projects/3.jpg")
     },
     {
-        name: "",
-        links: "",
+        name: "Work Day Scheduler",
+        links: "https://github.com/PGCranford/Work-Day-Scheduler- https://pgcranford.github.io/Work-Day-Scheduler-/",
         imagePath: require("../../assets/projects/4.jpg")
     },
     {
-        name: "",
-        links: "",
+        name: "Run Buddy",
+        links: "https://github.com/PGCranford/RB  https://lernantino.github.io/run-buddy/",
         imagePath: require("../../assets/projects/5.jpg")
     }
 ];
