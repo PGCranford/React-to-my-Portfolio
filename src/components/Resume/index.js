32278b9dd8e878690a7852bb99fd181e608ccfac
PGCranford <PGCranford@gmail.com>
2022-09-23 21:05:38 -0400

    styling

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,18 +1,19 @@
 import React from "react";
-import DownloadLink from "react-download-link";
+import styles from "./style.module.css";
 
 
 
 function Resume() {
 
     return (
-        <section>
-            <DownloadLink
-                label="Resume"
-                filename="PCResume.docx"
-                exportFile={() => "Resume Downloaded"}
-            />
-            <span>THIS WILL BE A RESUME</span>
+        <section className={styles["resume"]}>
+            <a className={styles["resume-link"]} href="https://docs.google.com/document/d/149r0ZQtq28xd6zszK5IMEyq1MGJ2kAdNmVzKNMYLhAY/edit?usp=sharing" >Resume</a>
+            <div className={styles["resume-list"]}>
+                <div>Language Proficiencies</div>
+                <div>Node.Js |  Express | JavaScript |  jQuery | React | GIT | GitHub </div>
+                <div>| MongoDB | MySQL | Handlebars | HTML | CSS | Bootstrap | Media Queries</div>
+                <div>| API’s | Microsoft | Heroku | RESTful API’s | Mongoose | JSON | Excel</div>
+            </div>
         </section>
     )
 };
