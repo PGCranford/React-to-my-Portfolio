05d230d726fd603c478909bcbaa32e8e935be22e
PGCranford <PGCranford@gmail.com>
2022-09-25 14:18:53 -0400

    about image

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,19 +6,20 @@ import beam from '../../assets/images/beam.jpg'
 function About() {
     return (
 
-        <section class="container card">
-            <img class="has-ration" src={beam} alt="beam" />
-            <div class="card-content is-overlay"  >
-                <div className={styles["bio"]}>
-                    <h1 class=" is-size-1-mobile has-text-centered" >Patrick Cranford - Web Developer</h1>
-                    <div  >
-                        <p class="is-size-1-mobile has-text-centered">
-                            Bringing my newly acquired full stack web develop certificate and years of retail and leadership experience to build an accessible and functional experience for any type of web application. Graduating from the Trilogy program through UNC-Charlotte with both front and back-end knowledge with a passion for back-end, I am excited to start my career in web development. Having spent time in group projects working on API’s and building MySQL databases to creating the CSS and HTML to display the information, making a functional and easy to use application drives me. Managing the team’s Github and creating Power Point presentations helped me become part of the team in unique way. I look forward to using the skills I have gathered throughout my life and more recently, Coding, to be a part of a fast-paced, driven and detailed web development team.
-                        </p>
-                    </div>
+
+        <section class="hero is-fullheight" className={styles["image"]}>
+            <div class="hero-body" >
+                <div class="container" className={styles["about"]}>
+                    <h1 class="title" >Patrick Cranford - Web Developer</h1>
+                </div>
+                <div class="" >
+                    <h2 class="subtitle">
+                        Bringing my newly acquired full stack web develop certificate and years of retail and leadership experience to build an accessible and functional experience for any type of web application. Graduating from the Trilogy program through UNC-Charlotte with both front and back-end knowledge with a passion for back-end, I am excited to start my career in web development. Having spent time in group projects working on API’s and building MySQL databases to creating the CSS and HTML to display the information, making a functional and easy to use application drives me. Managing the team’s Github and creating Power Point presentations helped me become part of the team in unique way. I look forward to using the skills I have gathered throughout my life and more recently, Coding, to be a part of a fast-paced, driven and detailed web development team.
+                    </h2>
                 </div>
             </div>
-        </section>
+
+        </section >
 
     )
 }
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -42,34 +42,36 @@ function Contact() {
 
     // JSX
     return (
-        <div class={styles["container"]}>
-            <section className={styles["contact-form"]}>
-                <img src={coverImage} className={styles["contact-image"]} style={{ width: "100%", height: "50%" }} alt="cover" />
-                <div className={styles["contact-block"]}>
-                    <h1 className={styles["contact-section"]} id="contact">Contact me</h1>
-                    <form id="contact-form" onSubmit={handleSubmit}>
-                        <div>
-                            <label htmlFor="name" className={styles["form-input"]}>Name:</label>
-                            <input type="text" name="name" defaultValue={name} onBlur={handleChange} className={styles["form-space"]} />
-                        </div>
-                        <div>
-                            <label htmlFor="email" className={styles["form-input"]}>Email address:</label>
-                            <input type="email" name="email" defaultValue={email} onBlur={handleChange} className={styles["form-space"]} />
-                        </div>
-                        <div>
-                            <label htmlFor="message" className={styles["form-input"]}>Message:</label>
-                            <textarea name="message" rows="7" cols="35" defaultValue={message} onBlur={handleChange} className={styles["form-space"]} />
-                        </div>
-                        {errorMessage && (
+        <section class="card">
+            <div class={styles["container"]}>
+                <section className={styles["contact-form"]} class="is-overlay">
+                    <img class="card-image" src={coverImage} width="100%" alt="cover" />
+                    <div className={styles["contact-block"]}>
+                        <h1 className={styles["contact-section"]} id="contact">Contact me</h1>
+                        <form action="mailto:PGCranford@gmail.com" method="get" enctype="text/plain" id="contact-form" onSubmit={handleSubmit}>
                             <div>
-                                <p className="error-text">{errorMessage}</p>
+                                <label htmlFor="name" className={styles["form-input"]}>Name:</label>
+                                <input type="text" name="name" defaultValue={name} onBlur={handleChange} className={styles["form-space"]} />
                             </div>
-                        )}
-                        <button id="button" type="submit">Submit</button>
-                    </form>
-                </div>
-            </section>
-        </div>
+                            <div>
+                                <label htmlFor="email" className={styles["form-input"]}>Email address:</label>
+                                <input type="email" name="email" defaultValue={email} onBlur={handleChange} className={styles["form-space"]} />
+                            </div>
+                            <div>
+                                <label htmlFor="message" className={styles["form-input"]}>Message:</label>
+                                <textarea name="message" rows="7" cols="35" defaultValue={message} onBlur={handleChange} className={styles["form-space"]} />
+                            </div>
+                            {errorMessage && (
+                                <div>
+                                    <p className="error-text">{errorMessage}</p>
+                                </div>
+                            )}
+                            <button id="button" type="submit">Submit</button>
+                        </form>
+                    </div>
+                </section >
+            </div >
+        </section>
     )
 }
 
