6d9ef878058292e9cad29899c23a1c51e9fca912
PGCranford <PGCranford@gmail.com>
2022-10-09 12:18:28 -0400

    project 3 added

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,62 +4,58 @@ import Project from "../Project";
 import styles from "./style.module.css";
 
 const projects = [
-    {
-        name: "Rent A Ride ",
-        link1: "https://github.com/PGCranford/Rent-A-Ride-",
-        link2: "https://sleepy-cove-56868.herokuapp.com/",
-        imagePath: require("../../assets/projects/0.jpg")
-    },
-    {
-        name: "Brews and Clues",
-        link1: "https://github.com/PGCranford/Brew-s-Clues-",
-        link2: "https://pgcranford.github.io/Brew-s-Clues-/ ",
-        imagePath: require("../../assets/projects/1.jpg")
-    },
-    {
-        name: "Note Taker",
-        link1: "https://github.com/PGCranford/Take-a-note-Leave-a-note-",
-        link2: "https://dry-cove-43814.herokuapp.com/ ",
-        imagePath: require("../../assets/projects/2.jpg")
-    },
-    {
-        name: "Horiseon",
-        link1: "https://github.com/PGCranford/Horiseon-Code-Refactor-",
-        link2: "https://pgcranford.github.io/MOD1-/",
-        imagePath: require("../../assets/projects/3.jpg")
-    },
-    {
-        name: "Work Day Scheduler",
-        link1: "https://github.com/PGCranford/Work-Day-Scheduler-",
-        link2: "https://pgcranford.github.io/Work-Day-Scheduler-/",
-        imagePath: require("../../assets/projects/4.jpg")
-    },
-    {
-        name: "Run Buddy",
-        link1: "https://github.com/PGCranford/RB-",
-        link2: " https://lernantino.github.io/run-buddy/",
-        imagePath: require("../../assets/projects/5.jpg")
-    }
+  {
+    name: "Rent A Ride ",
+    link1: "https://github.com/PGCranford/Rent-A-Ride-",
+    link2: "https://sleepy-cove-56868.herokuapp.com/",
+    imagePath: require("../../assets/projects/0.jpg"),
+  },
+  {
+    name: "Brews and Clues",
+    link1: "https://github.com/PGCranford/Brew-s-Clues-",
+    link2: "https://pgcranford.github.io/Brew-s-Clues-/ ",
+    imagePath: require("../../assets/projects/1.jpg"),
+  },
+  {
+    name: "Hyieia Fitness ",
+    link1: "https://github.com/PGCranford/Hygieia-Fitness",
+    link2: "https://hygieia-fitness-p3g6.herokuapp.com/ ",
+    imagePath: require("../../assets/projects/P3.jpg"),
+  },
+  {
+    name: "Horiseon",
+    link1: "https://github.com/PGCranford/Horiseon-Code-Refactor-",
+    link2: "https://pgcranford.github.io/MOD1-/",
+    imagePath: require("../../assets/projects/3.jpg"),
+  },
+  {
+    name: "Work Day Scheduler",
+    link1: "https://github.com/PGCranford/Work-Day-Scheduler-",
+    link2: "https://pgcranford.github.io/Work-Day-Scheduler-/",
+    imagePath: require("../../assets/projects/4.jpg"),
+  },
+  {
+    name: "Note Taker",
+    link1: "https://github.com/PGCranford/Take-a-note-Leave-a-note-",
+    link2: "https://dry-cove-43814.herokuapp.com/ ",
+    imagePath: require("../../assets/projects/2.jpg"),
+  },
 ];
 
 const Portfolio = () => {
-    return (
-
-        <div className={styles["project-display"]} >
-            {
-                projects.map(({ name, link1, link2, imagePath }) => (
-                    <Project
-                        imagePath={imagePath}
-                        name={name}
-                        link1={link1}
-                        link2={link2}
-                        key={name}
-                    />
-                ))
-            }
-        </div >
-
-    );
+  return (
+    <div className={styles["project-display"]}>
+      {projects.map(({ name, link1, link2, imagePath }) => (
+        <Project
+          imagePath={imagePath}
+          name={name}
+          link1={link1}
+          link2={link2}
+          key={name}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Portfolio;
