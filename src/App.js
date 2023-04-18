930eee29a57a03d62a67a229ae60ba62f182e2d6
PGCranford <PGCranford@gmail.com>
2023-04-18 19:09:47 -0400

    home removed

--- src/App.js
+++ src/App.js
@@ -1,59 +1,34 @@
-import React, { Component } from 'react';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import About from './components/About'
-import Home from './components/Home'
-import Resume from './components/Resume'
+import React, { Component } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import About from "./components/About";
 
-import './App.css';
+import Resume from "./components/Resume";
 
+import "./App.css";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
+      <Router className="main-app">
+        <Header></Header>
 
-      <Router className="main-app" >
-
-    
-          <Header></Header>
-
-          <main>
-            <div className="cover-image">
-
-              <Routes>
-                <Route
-                  exact path="/Contact"
-                  element={<Contact />}
-                />
-                <Route
-                  exact path="/About"
-                  element={<About />}
-                />
-                <Route
-                  exact path="/Portfolio"
-                  element={<Portfolio />}
-                />
-                <Route
-                  exact path="/Resume"
-                  element={<Resume />}
-                />
-                <Route
-                  exact path='/Home'
-                  element={<Home />}
-                />
-              </Routes>
-            </div>
-          </main>
-          <Footer></Footer>
+        <main>
+          <div className="cover-image">
+            <Routes>
+              <Route exact path="/Contact" element={<Contact />} />
+              <Route exact path="/About" element={<About />} />
+              <Route exact path="/Portfolio" element={<Portfolio />} />
+              <Route exact path="/Resume" element={<Resume />} />
+            </Routes>
+          </div>
+        </main>
+        <Footer></Footer>
       </Router>
-
     );
   }
 }
