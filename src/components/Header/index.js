c209c3fcf1e35694630ba5634b6c2ccd9ed193e2
PGCranford <PGCranford@gmail.com>
2023-04-18 18:23:05 -0400

    home link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navigation from "../Navigation";
 import styles from "./style.module.css";
+// import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className={styles["header"]}>
+      {/* <Link to={`/Home`}>
+        <li className={styles["links"]}>🍌Patrick Cranford </li>
+      </Link> */}
       <h2>
-        <a id="link" href="/">
+        <a id="link" href="../../App.js">
           <span role="img" aria-label="banana">
             🍌{" "}
           </span>
