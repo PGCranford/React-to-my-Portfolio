baabc290c856d71a273d5c06ab66ad25255fc3e0
PGCranford <PGCranford@gmail.com>
2023-04-18 18:36:16 -0400

    Name link working

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import Navigation from "../Navigation";
 import styles from "./style.module.css";
-// import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className={styles["header"]}>
-      {/* <Link to={`/Home`}>
-        <li className={styles["links"]}>🍌Patrick Cranford </li>
-      </Link> */}
       <h2>
-        <a id="link" href="../../App.js">
+        <a id="link" href="src\App.js">
           <span role="img" aria-label="banana">
             🍌{" "}
           </span>
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,40 +1,26 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import styles from "./style.module.css";
 
-
-
 function Navigation() {
-    return (
-        <nav>
-            <ul className={styles["nav-row"]}>
-                <Link
-                    to={`/About`}
-                >
-                    <li className={styles["links"]} > About Me
-                    </li>
-                </Link>
-                <Link
-                    to={'/Portfolio'}
-                >
-                    <li className={styles["links"]}> Portfolio
-                    </li>
-                </Link>
-                <Link
-                    to={'/Resume'}
-                >
-                    <li className={styles["links"]}>Resume
-                    </li>
-                </Link>
-                <Link
-                    to={'/Contact'}
-                >
-                    <li className={styles["links"]}> Contact
-                    </li>
-                </Link>
-            </ul>
-        </nav >
-    );
+  return (
+    <nav>
+      <ul className={styles["nav-row"]}>
+        <Link to={`/About`}>
+          <li className={styles["links"]}> About Me</li>
+        </Link>
+        <Link to={"/Portfolio"}>
+          <li className={styles["links"]}> Portfolio</li>
+        </Link>
+        <Link to={"/Resume"}>
+          <li className={styles["links"]}>Resume</li>
+        </Link>
+        <Link to={"/Contact"}>
+          <li className={styles["links"]}> Contact</li>
+        </Link>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navigation;
