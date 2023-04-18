49df297fc5307dae3828c2e1cc764b7a6d523ca5
PGCranford <PGCranford@gmail.com>
2023-04-18 19:22:30 -0400

    site working

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import coverImage from '../../assets/images/curtains.jpg'
+import styles from "./style.module.css";
+
+
+
+function Home() {
+
+    return (
+        <img src={coverImage} className={styles["cover-image"]} style={{ width: "100%", height: "80%" }} alt="cover" />
+    );
+}
+
+export default Home;
