cb45d4a36333f437070ec111ec4444c0cd534cbd
PGCranford <PGCranford@gmail.com>
2023-04-18 16:58:55 -0400

    navigation work

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,22 +2,21 @@ import React from "react";
 import Navigation from "../Navigation";
 import styles from "./style.module.css";
 
-
 function Header() {
-
-
-    return (
-        <header className={styles["header"]}>
-            <h2 >
-                <a id="link" href="/">
-                    <span role="img" aria-label="banana">🍌 </span>Patrick Cranford
-                </a>
-                <Navigation
-                ></Navigation>
-            </h2>
-        </header >
-
-    );
+  return (
+    <header className={styles["header"]}>
+      <h2>
+        <a id="link" href="/">
+          <span role="img" aria-label="banana">
+            🍌{" "}
+          </span>
+          Patrick Cranford
+        </a>
+        {/* <Navigation
+                ></Navigation> */}
+      </h2>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
